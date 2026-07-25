f5cae6e49ecfe35383ad03b624ab1a4975192cd8
amara-wd <amarawork2000@gmail.com>
2026-07-25 15:44:16 +0500

    Initial project setup

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">
+      Bundle Builder
+    </h1>
+
+   
+      <section id="spacer"></section>
+    </>
+  )
+}
+
+export default App
